4206947e3225e7aca5a99a392c9a70a2bb7678ea
Paolo Pisati <paolo.pisati@canonical.com>
2025-08-20 12:06:43 +0200

    UBUNTU: [Packaging] remove do_extras_package
    Signed-off-by: Paolo Pisati <paolo.pisati@canonical.com>

--- debian.oracle/rules.d/amd64.mk
+++ debian.oracle/rules.d/amd64.mk
@@ -17,7 +17,6 @@ do_tools_perf_jvmti	= true
 do_tools_bpftool	= false
 do_tools_x86		= true
 do_tools_hyperv		= false
-do_extras_package	= true
 do_tools_acpidbg	= true
 do_zfs			= true
 disable_d_i		= true
